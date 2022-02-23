49ac45cb7d1df38479a6316f4e2d568aafda8ff5
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-02-23 23:02:07 +0600

    new

--- JS/home.js
+++ JS/home.js
@@ -26,27 +26,22 @@
 }*/
 
 
-function sort()
-{
+function sort() {
     let x = document.getElementById("sort1");
     let y = x.classList;
 
-    for(let i=0; i<y.length; i++)
-    {
-        if(y[i] == "uil-sort")
-        {
+    for (let i = 0; i < y.length; i++) {
+        if (y[i] == "uil-sort") {
             y.remove(y[i]);
             y.add("uil-sort-amount-down");
             return;
         }
-        else if(y[i] == "uil-sort-amount-down")
-        {
+        else if (y[i] == "uil-sort-amount-down") {
             y.remove(y[i]);
             y.add("uil-sort-amount-up");
             return;
         }
-        else if(y[i] == "uil-sort-amount-up")
-        {
+        else if (y[i] == "uil-sort-amount-up") {
             y.remove(y[i]);
             y.add("uil-sort");
             return;
@@ -55,8 +50,7 @@ function sort()
 }
 
 
-
-function openModal() {
+/* function openModal() {
     document.querySelector(".modal").classList.add("open");
 }
 
@@ -64,26 +58,22 @@ function closeModal() {
     document.querySelector(".modal").classList.remove("open");
 }
 
-
-function fav(fav_icon)
-{
+function fav(fav_icon) {
     fav_icon.classList.toggle('uis-star')
-}
+} */
 
 
-function search()
-{
+function search() {
     let sort = document.getElementById('sort');
     let set = document.getElementById('set');
     let pro = document.getElementById('pro');
     let sear = document.getElementById('srch');
     let search_text = document.getElementById('search-text');
-    
+
     let search_bar = document.getElementById('search-bar');
 
 
-    if(sear.className == "actv")
-    {
+    if (sear.className == "actv") {
         sear.style.display = "none";
         search_bar.style.display = "flex";
         search_bar.style.transition = "0.6s";
@@ -93,15 +83,27 @@ function search()
         pro.style.display = "none";
     }
 
-    else
-    {
+    else {
         sear.style.display = "block";
         search_bar.style.display = "none";
         sear.className = "actv";
         sort.style.display = "block";
         set.style.display = "block";
         pro.style.display = "block";
-
         search_text.value = "";
     }
 }
+
+
+const popupScreen = document.querySelector(".popup-screen");
+/* const popupBox = document.querySelector(".popup-box"); */
+const closeBtn = document.querySelector(".close-btn");
+const openBtn = document.querySelector(".open-btn");
+
+openBtn.addEventListener("click", () => {
+    popupScreen.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    popupScreen.classList.remove("active");
+});
